499f8b8703bb927710521fceda7f0594cc3d8e2e
AC_Marvel <gil_1_10@hotmail.com>
2023-10-11 18:48:19 +0200

    Criar função que desenha, apaga (burracha), limpa a tela

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,22 +5,97 @@ const ctx = canvas.getContext("2d")
 
 const inputColor = document.querySelector(".input__color")
 const tools = document.querySelectorAll(".button__tool")
-const sizeButtons = document.querySelector(".button_size")
-const buttoClear = document.querySelector(".button_clear")
+const sizeButtons = document.querySelectorAll(".button__size")
+const buttoClear = document.querySelector(".button__clear")
 
-let brushSize = 50
+let brushSize = 20
 
-canvas.addEventListener("mousedown", (event) => {
-    const { clientX, clientY } = event 
-    draw(clientX, clientY)
+let isPainting = false
+
+let activeTool = "brush"
+
+inputColor.addEventListener("change", ({ target }) => {
+    ctx.fillStyle = target.value
+})
+
+canvas.addEventListener("mousedown", ({ clientX, clientY}) => {
+    isPainting = true
+    if (activeTool == "brush") {
+        draw(clientX, clientY)
+    }
+
+    if (activeTool == "rubber") {
+        erase(clientX, clientY)
+    } 
+})
+
+canvas.addEventListener("mousemove", ({ clientX, clientY}) => {
+    if (isPainting) {
+        if (activeTool == "brush") {
+            draw(clientX, clientY)    
+        }
+        if (activeTool == "rubber") {
+            erase(clientX, clientY)
+        } 
+    }
+})
+
+canvas.addEventListener("mouseup", ({ clientX, clientY}) => {
+    isPainting = false
 })
 
 const draw = (x, y) => {
-    ctx.fillStyle = "#000"
-    ctx.fillRect(
+    ctx.globalCompositeOperation = "source-over"
+    ctx.beginPath()
+    ctx.arc(
         x - canvas.offsetLeft, 
         y - canvas.offsetTop, 
-        brushSize, 
-        brushSize)
+        brushSize / 2, 
+        0, 
+        2 * Math.PI
+        )
+    ctx.fill()
 }
 
+const erase = (x, y) => {
+    ctx.globalCompositeOperation = "destination-out"
+    ctx.beginPath()
+    ctx.arc(
+        x - canvas.offsetLeft,
+        y - canvas.offsetTop,
+        brushSize / 2,
+        0,
+        2 * Math.PI
+    )
+    ctx.fill()
+}
+
+const selectTool = ({target}) => {
+    const selectedTool = target.closest("button")
+    const action = selectedTool.getAttribute("data-action")    
+    if (action) {
+        tools.forEach((tool) => tool.classList.remove("active"))
+        selectedTool.classList.add("active")
+        activeTool = action
+    }
+}
+
+const selectSize = ({target}) => {
+    const selectedTool = target.closest("button")
+    const size = selectedTool.getAttribute("data-size")
+    sizeButtons.forEach((tool) => tool.classList.remove("active"))
+    selectedTool.classList.add("active")
+    brushSize = size
+}
+
+tools.forEach((tool) => {
+    tool.addEventListener("click", selectTool)
+})
+
+sizeButtons.forEach((button) => {
+    button.addEventListener("click", selectSize)
+})
+
+buttoClear.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+})
